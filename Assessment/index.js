a0a3fd1a2b5b8a712ffc881434ddbd73d445b7e7
KshitizSrivastav <kshitizsr02@gmail.com>
2024-10-21 12:45:32 +0530

    Assessment work

---
+++ Assessment/index.js
@@ -0,0 +1,29 @@
+const button = document.getElementById("btn");
+const container = document.getElementById("container");
+
+function displayBulb(){
+    const textarea = document.getElementsByTagName("textarea");
+    const bulbs = textarea[0].value
+    console.log(bulbs);
+    for(let i=0;i<bulbs;i++){
+        const bulb = document.createElement("img");
+        const check = document.createElement("input");
+        check.type="checkbox";
+        console.log(check);
+        
+        bulb.src = "https://pics.clipartpng.com/Light_Bulb_PNG_Clip_Art-2102.png";
+        bulb.setAttribute("height","150px");
+        bulb.setAttribute("width","75px");
+        container.appendChild(check);
+        container.appendChild(bulb);
+    }
+}
+
+function quantity(){
+    container.innerHTML = "<textarea name='' id='textarea' placeholder='Enter number of bulbs'></textarea>";
+    const submit = document.createElement("button");
+    submit.innerHTML = "Submit";
+    container.appendChild(submit);
+    submit.addEventListener("click",displayBulb);
+}
+button.addEventListener("click",quantity);
