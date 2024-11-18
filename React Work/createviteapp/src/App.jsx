6fdb2ba399f0c99919084dfe310670e5727a83a0
KshitizSrivastav <139064011+KshitizSrivastav@users.noreply.github.com>
2024-11-18 07:36:05 +0000

    Updates

--- React Work/createviteapp/src/App.jsx
+++ React Work/createviteapp/src/App.jsx
@@ -1,26 +1,56 @@
 import React from 'react'
 import Student from './Student';
 import logo from './abeslogo.jpg';
+import StudentState from './StudentState';
 
 function App() {
-  let a=12;
-  const mystyle={
-    backgroundColor:'Red',
-    color: 'cyan' , 
-    display: 'flex' ,
-  }           
-  return (
-    <div style={mystyle}>ABES Engineering College
-    <div style={mystyle}>{a}</div>
-    <div style={mystyle}>
-      <Student college="ABES Engineering College"
-      pic={<img src={logo} height={200} width={200} />}
-      name="Kshitiz" roll="123" branch="CSE"/>
+  // let a=12;
+  // const mystyle={
+  //   backgroundColor:'Red',
+  //   color: 'cyan' , 
+  //   display: 'flex' ,
+  // }     
+  //  const studentData=[{
+  //   college:"ABES Engineering College",
+  //   name:"Rahul",
+  //   pic:"https://commondatastorage.googleapis.com/codeskulptor-demos/riceracer_assets/img/car_4.png",
+  //   roll:"1234",
+  //   branch:"CSE"
+  //  },
+  //  {
+  //   college:"ABES Engineering College",
+  //   name:"Rohan",
+  //   pic:"https://commondatastorage.googleapis.com/codeskulptor-demos/riceracer_assets/img/car_4.png",
+  //   roll:"5678",
+  //   branch:"CSE-AIML"
+  //  },
+  //  {
+  //   college:"ABES Engineering College",
+  //   name:"Shya",
+  //   pic:"https://commondatastorage.googleapis.com/codeskulptor-demos/riceracer_assets/img/car_4.png",
+  //   roll:"91011",
+  //   branch:"CS"
+  //  }
+  //  ]
 
-      <Student college="ABES Engineering College"
-      pic={<img src={logo} height={200} width={200} />}
-      name="Amit" roll="456" branch="CSE-AIML"/>
-    </div>
+  // return (
+  //   <div style={mystyle}>ABES Engineering College
+  //   <div style={mystyle}>{a}</div>
+  //   <div style={mystyle}>
+  //     <Student data={studentData[1]} />
+  //     {/* <Student college="ABES Engineering College"
+  //     pic={<img src={logo} height={200} width={200} />}
+  //     name="Kshitiz" roll="123" branch="CSE"/> */}
+
+  //     {/* <Student //college="ABES Engineering College"
+  //     pic={<img src={logo} height={200} width={200} />}
+  //     name="Amit" roll="456" branch="CSE-AIML"/> */}
+  //   </div>
+  //   </div>
+  //)
+  return (
+    <div>
+      <StudentState/>
     </div>
   )
 }
--- React Work/createviteapp/src/Student.jsx
+++ React Work/createviteapp/src/Student.jsx
@@ -1,19 +1,24 @@
 import React from 'react'
 import './student.css';
-function Student(props) {
+function Student({data}) {
   return (
     <div className='idcard'>
+      {/* {
+        JSON.stringify(data)
+      } */}
         <table>
             <tbody>
-                <tr style={{backgroundColor:'Red',color: 'cyan'}}><td colSpan={2}>{props.college}</td></tr>
-                <tr><td colSpan={2}>{props.pic}</td></tr>
-                <tr><td>Name:</td><td>{props.name}</td></tr>
-                <tr><td>Roll No:</td><td>{props.roll}</td></tr>
-                <tr><td>Branch:</td><td>{props.branch}</td></tr>
+                <tr style={{backgroundColor:'Red',color: 'cyan'}}><td colSpan={2}>{data.college}</td></tr>
+                <tr><td colSpan={2}><img src={data.pic} height={200} width={200}/></td></tr>
+                <tr><td>Name:</td><td>{data.name}</td></tr>
+                <tr><td>Roll No:</td><td>{data.roll}</td></tr>
+                <tr><td>Branch:</td><td>{data.branch}</td></tr>
             </tbody>
         </table>
     </div>
   )
 }
-
+// Student.defaultProps={
+//   college: "AKG Engineering College"
+// }
 export default Student
