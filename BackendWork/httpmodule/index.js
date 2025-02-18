a44712eb408878395ffcb753d3486d83a6a338df
KshitizSrivastav <139064011+KshitizSrivastav@users.noreply.github.com>
2025-02-18 04:05:56 +0000

    Postman API work

--- BackendWork/httpmodule/index.js
+++ BackendWork/httpmodule/index.js
@@ -2,9 +2,24 @@ const http=require('http');
 const PORT=2524;
 const server=http.createServer((req,res)=>{
     console.log("Inside the server body");
+    if (req.url=='/' && req.method=="GET"){
     res.setHeader('Content-Type',"text/html");
+    //console.log(Object.keys(req));
+    //console.log(req.url+" "+req.method);
     res.write("<div style='background-color:coral'><h2 style='color:red'>Welcome to the Node server</h2></div>");
-    res.end("<h2>Server has ended</h2>");
+    res.end("<h2>Server has ended</h2>");}
+    // if(req.url=='/show' && req.method=='GET'){
+    //     res.setHeader('Content-Type','application/json');
+    //     res.end(JSON.stringify({
+    //         msg:"Successfully hit the API"
+    //     }))
+    // }
+    if(req.url=='/show' && req.method=='POST'){
+        res.setHeader('Content-Type','application/json');
+        res.end(JSON.stringify({
+            msg:"Successfully hit the post API @/show"
+        }))
+    }
 })
 server.listen(PORT,()=>{
     console.log("Server is running on "+PORT);
