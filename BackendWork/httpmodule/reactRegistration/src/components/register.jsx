d33b99cee5b88f0f4a63b64a038ebf50a37b85c0
KshitizSrivastav <139064011+KshitizSrivastav@users.noreply.github.com>
2025-03-04 03:52:59 +0000

    Update

---
+++ BackendWork/httpmodule/reactRegistration/src/components/register.jsx
@@ -0,0 +1,12 @@
+// import React from 'react'
+import { Component } from 'react'
+
+export class register extends Component {
+  render() {
+    return (
+      <div>register</div>
+    )
+  }
+}
+
+export default register
